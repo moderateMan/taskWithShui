5f68b5848a2999ce98d86faf36e1273f0de8d69a
pan xiaoliang <1354383179@qq.com>
2024-03-17 15:23:17 +0800

    feat: 评论字段修改

--- h5/src/pages/review/index.tsx
+++ h5/src/pages/review/index.tsx
@@ -45,7 +45,7 @@ export default function Review() {
             </span>
           </div>
         </div>
-        <Form.Item name="comment">
+        <Form.Item name="content">
           <TextArea
             className={styles["textarea"]}
             placeholder="请您填写对视频的评价吧"
